3b81238325aed7d766a952c9ca99d894ef59b668
Preston Garrison III <pg3@flightone.com>
2023-02-16 13:45:57 -0500

    testing with HTML

---
+++ k6bench.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export const options = {
+  vus: 1000,
+  duration: '30s',
+};
+export default function () {
+  http.get('http://localhost:7878/sleep');
+  sleep(1);
+}
