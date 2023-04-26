01aef52a1b11c3764fa3c946fa15279216c4d27d
Preston Garrison III <pg3@flightone.com>
2023-04-26 11:21:05 -0400

    feat: handle reading manually and allow long lasting connections

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 use http::MimeType;
+use log;
 use request::Request;
 use response::Response;
-use routes::{Routes, ResolveFunction};
-use std::{path::PathBuf, sync::Arc};
+use routes::{ResolveFunction, Routes};
+use std::{fmt::format, path::PathBuf, sync::Arc};
 use tokio::{
     self, fs,
-    io::{AsyncBufReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
+    sync::Mutex,
     sync::RwLock,
 };
-use log;
 
 pub mod http;
 pub mod methods;
@@ -94,8 +95,67 @@ impl Server {
             let mut connection = self.accept().await?;
 
             tokio::spawn(async move {
+                let mut request_str = String::new();
+                loop {
+                    let mut buffer = vec![0; 1024]; //Vector to avoid buffer on stack
+                    match connection.stream.read(&mut buffer).await {
+                        Ok(0) => {
+                            log::debug!("Connection Terminated by client");
+                            break;
+                        }
+                        Ok(n) => {
+                            //got some bytes append them and see if we need to do any proccessing
+                            for i in 0..n {
+                                request_str.push(buffer[i] as char);
+                                let request_result =
+                                    request::Request::from_string(request_str.clone());
+                                match request_result {
+                                    Ok(r) => {
+                                        let response = Self::route(&r, &connection).await;
+                                        connection.write_response(response).await.unwrap();
+                                        request_str.clear();
+                                    }
+                                    Err(e) => match e {
+                                        request::Error::InvalidString
+                                        | request::Error::MissingBlankLine => {
+                                            //Parital response keep reading
+                                        }
+                                        _ => {
+                                            let error_res = format!("400 bad request: {}", e);
+                                            let response = Response::error(
+                                                http::StatusCode::ErrBadRequest,
+                                                error_res.into(),
+                                            );
+                                            if let Err(err) =
+                                                connection.write_response(response).await
+                                            {
+                                                log::error!(
+                                                    "Error Writing Data: {}",
+                                                    err.to_string()
+                                                );
+                                            }
+                                        }
+                                    },
+                                }
+                            }
+                        }
+                        Err(err) => {
+                            log::error!("Socket read error: {}", err.to_string());
+                            break;
+                        }
+                    }
+                }
+            });
+        }
+    }
+    pub async fn old_serve(&self) -> tokio::io::Result<()> {
+        loop {
+            let mut connection = self.accept().await?;
+
+            tokio::spawn(async move {
                 let mut buf_reader = tokio::io::BufReader::new(&mut connection.stream);
                 let mut request_str = "".to_owned();
+
                 loop {
                     let mut line: String = "".to_string();
                     let result = buf_reader.read_line(&mut line).await;
@@ -117,17 +177,15 @@ impl Server {
                         let response = Self::route(&r, &connection).await;
                         connection.write_response(response).await.unwrap();
                     }
-                    Err(e) => {
-                        match e {
-                            _ => {
-                                let response = Response::error(
-                                    http::StatusCode::ErrBadRequest,
-                                    "400 Bad Request".into(),
-                                );
-                                connection.write_response(response).await.unwrap();
-                            }
+                    Err(e) => match e {
+                        _ => {
+                            let response = Response::error(
+                                http::StatusCode::ErrBadRequest,
+                                "400 Bad Request".into(),
+                            );
+                            connection.write_response(response).await.unwrap();
                         }
-                    }
+                    },
                 }
             });
         }
@@ -137,7 +195,8 @@ impl Server {
         let blocking = tokio::task::spawn_blocking(move || {
             let result = func(&request);
             return result;
-        }).await;
+        })
+        .await;
         //FIXME: return error response intead of unwap
         return Response::from(blocking.unwrap());
     }
